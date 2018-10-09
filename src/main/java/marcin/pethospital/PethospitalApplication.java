f6c49f0cbff72a1173cc909ae1891eb1b0d232ea
Marcin Kloskowski <kloskowski@gmail.com>
2018-10-09 10:52:47 +0200

    initial commit

---
+++ src/main/java/marcin/pethospital/PethospitalApplication.java
@@ -0,0 +1,12 @@
+package marcin.pethospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PethospitalApplication.class, args);
+    }
+}
